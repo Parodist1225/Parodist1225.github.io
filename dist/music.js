67550b7dd2bb6b6a6da854a46ee97e3208fcc705
Parodist1225 <2633104407@qq.com>
2023-06-05 16:57:30 +0800

    Site updated: 2023-06-05 16:57:29

--- dist/music.js
+++ dist/music.js
@@ -6,14 +6,14 @@ const ap = new APlayer({
   audio: [
     {
       name: "千年之羽（符华印象曲）",
-      artist: '小林未郁',
+      artist: '小林未郁',//530986486
       url: 'http://parodist1225.top/static/music/小林未郁-千年之羽.mp3',
       cover: 'http://parodist1225.top/static/music/千年之羽.jpg',
     },
     {
       name: 'Moon Halo',
       artist: '茶理理,TetraCalyx,Hanser',
-      url: 'http://parodist1225.top/static/music/MoonHalo.mp3',
+      url: 'http://music.163.com/song/media/outer/url?id=1859652717.mp3',
       cover: 'http://192.168.111.242:8081/IXP46078c365a6cfdefa7c28f4479e73221179b16fae0c64_10.11.178.133/p1.music.126.net/ciLKATqflV2YWSV3ltE2Kw==/109951166159281275.jpg?param=130y130',
     },
     {
@@ -28,8 +28,38 @@ const ap = new APlayer({
       url: 'http://music.163.com/song/media/outer/url?id=1990743306.mp3',
       cover: 'https://p2.music.126.net/-LHyMtXmo-yDf-eqwb8ThA==/109951168143056350.jpg?param=90y90',
     },
-  
-
+    {
+      name: '紙一重',
+      artist: 'Uru',
+      url: 'http://music.163.com/song/media/outer/url?id=2034495416.mp3',
+      cover: 'https://p2.music.126.net/GC_78ghJTt00r0QIEn_7IA==/109951168504812659.jpg?param=90y90',
+    },
+    {
+      name: 'Merry Christmas Mr. Lawrence',
+      artist: '坂本龍一',
+      url: 'http://music.163.com/song/media/outer/url?id=4899152.mp3',
+      cover: 'https://p1.music.126.net/woiqainQI-orV_RuUuOVRw==/716881581353216.jpg?param=130y130',
+    },
+    {
+      name: '極楽浄土',
+      artist: 'GARNiDELiA',
+      url: 'http://music.163.com/song/media/outer/url?id=411907897.mp3',
+      cover: 'https://p1.music.126.net/T01LxKNXKebyWE0GBPzyeA==/109951167953959333.jpg?param=130y130',
+    },
+    {
+      name: 'すずめ feat.十明',
+      artist: 'RADWIMPS / 十明',
+      url: 'http://music.163.com/song/media/outer/url?id=1984758339.mp3',
+      cover: 'https://p1.music.126.net/jHIFF-TBXfG3hbh4IOnudQ==/109951167911814845.jpg?param=130y130',
+    },
 
   ]
 });
+/*
+{
+  name: '',
+  artist: '',
+  url: 'http://music.163.com/song/media/outer/url?id=.mp3',
+  cover: '',
+},
+*/ 
