273812ca23ef4ee59bdff0ca2053e9bc1c06fecf
Parodist1225 <2633104407@qq.com>
2023-06-06 00:58:43 +0800

    Site updated: 2023-06-06 00:58:42

---
+++ dist/modify.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * 在页面插入新顶部图
+ * @param {cheerio.Root} $ Root
+ */
+function insertTopImg($) {
+    let header = $('#page-header');
+    if (header.length === 0) return;
+    let background = header.css('background-image');
+    if (!background) return;
+    $('#post, #page, #archive, #tag, #category').prepend(`<div class="top-img" style="background-image: ${background};"></div>`);
+}
+
+hexo.extend.filter.register('after_render:html', function(str, data) {
+    let $ = load(str, {
+        decodeEntities: false
+    });
+    insertTopImg($);
+    return $.html();
+});
